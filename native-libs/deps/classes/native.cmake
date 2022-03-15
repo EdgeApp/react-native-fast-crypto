e04baca42606a9fcf4f179bb5c798cc7326d1968
William Swanson <swansontec@gmail.com>
2022-03-15 16:52:17 -0700

    Remove unused files

--- native-libs/deps/classes/native.cmake
+++
@@ -1,9 +0,0 @@
-# Where is the target environment:
-set(CMAKE_FIND_ROOT_PATH  "${CMAKE_INSTALL_PREFIX}")
-
-# Search for programs in the build host directories:
-set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-
-# Search for libraries and headers in the target directories:
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
